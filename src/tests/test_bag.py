a80c6f4949fad2324e67f6b7f2fa06642a8b544b
Orn Gudjonsson <orngudjonsson@gmail.com>
2016-02-02 15:54:23 +0000

    added another test for bag enumaration

--- src/tests/test_bag.py
+++ src/tests/test_bag.py
@@ -7,10 +7,22 @@ import bag
 
 class BagTests(unittest.TestCase):
     def test_bagify_empty(self):
+        """Test enumaration when there is no previous bag"""
         string = "dog cat cow"
         expected = set([frozenset(['dog', 'cat']),
                     frozenset(['dog', 'cow']),
                     frozenset(['cat', 'cow'])])
         result = bag.bagify(string, {})
         self.assertEqual(expected, result)
+
+    def test_bagify_nonempty(self):
+        """tests enumeration when there are previous bags"""
+        string = "dog cat cow"
+        oldbags = set([frozenset(['dog', 'cow']),
+                    frozenset(['dog', 'chicken']),])
+        expected = set([frozenset(['dog', 'cat']),
+                    frozenset(['cat', 'cow'])])
+        result = bag.bagify(string, oldbags)
+        self.assertEqual(expected, result)
+
 
