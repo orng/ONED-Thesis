6469363274ce457db421dec1c487d22b43bf2ee7
Orn Gudjonsson <orngudjonsson@gmail.com>
2016-02-11 16:47:30 +0000

    Added scrapy project for scraping nba articles, implemented with espn for now

---
+++ src/nba/nba/items.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class NbaItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    #title = scrapy.Field()
+    text = scrapy.Field()
+    #date = scrapy.Field()
+    url = scrapy.Field()
---
+++ src/nba/nba/spiders/espn_spider.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env
+
+import scrapy 
+from nba.items import NbaItem
+
+class EspnSpider(scrapy.Spider):
+    name='espn'
+    allowed_domains = ['espn.go.com']
+    start_urls = ['http://espn.go.com/nba/',
+            ]
+    def parse(self, response):
+        for url in response.xpath('//a[@class=" realStory"]/@href').extract():
+            """
+            Grab all the articles on the page
+            """
+            yield scrapy.Request(response.urljoin(url), callback=self.parse_article)
+
+    def parse_article(self, response):
+        contentlist = response.xpath('//div[@class="article-body"]/p/text()').extract()
+        article = "".join(contentlist)
+        item = NbaItem()
+        item['text'] = article
+        item['url'] = response.url
+        yield item
+
