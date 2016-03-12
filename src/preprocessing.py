1712b4152f28337ce7d678fbce0868b418faca8e
Orn Gudjonsson <orngudjonsson@gmail.com>
2016-03-12 19:00:59 +0000

    Added functions to use for ignoring frequent words to preprocessing module

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -4,6 +4,7 @@
 
 __author__ = "Orn Gudjonsson"
 
+from collections import defaultdict
 from stemming.porter2 import stem
 from nltk.corpus import stopwords
 import regex as re
@@ -32,10 +33,47 @@ def remove_numbers(words):
     return filter(lambda x: not x.isdigit(), words)
     
 
+def to_wordlist(string):
+    return remove_stopwords(
+            stem_words(
+                remove_numbers(
+                    remove_punctuation(
+                        string))))
+
+
 def preprocess(string):
-    return remove_duplicates(
-            remove_stopwords(
-                stem_words(
-                    remove_numbers(
-                        remove_punctuation(
-                            string)))))
+    return remove_duplicates(to_wordlist(string))
+
+
+
+def word_frequency(wordList):
+    """
+    Given a list of words, returns a dict mapping words 
+    to how often they appear in the list
+    """
+    freqDict = defaultdict(int)
+    for word in wordList:
+        freqDict[word] += 1
+    return freqDict
+
+def filter_common(wordList, frequencyDict, n):
+    frequencyTuples = sorted(frequencyDict.items(), key = lambda x: x[1])
+    totalCount = sum(frequencyDict.values())
+    freqList = [(x, y/float(totalCount)) for (x,y) in frequencyTuples]
+    totalFreq = 0
+    mostFrequent = []
+    #TODO: perhaps limit to a certain amount of elements in mostFrequent
+    while totalFreq < n and len(freqList) > 1:
+        nextTuple = freqList[0]
+        mostFrequent.append(nextTuple[0])
+        totalFreq += nextTuple[1]
+        freqList = freqList[1:]
+    return filter(lambda x: x in mostFrequent, wordList)
+        
+
+
+        
+
+
+
+
